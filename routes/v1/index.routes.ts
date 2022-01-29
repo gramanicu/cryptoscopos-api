c109d706ec2d2eb3ed7c1580d340f5245e3cfb69
Grama Nicolae <gramanicu@gmail.com>
2022-01-29 18:35:30 +0200

    Small router change

--- routes/v1/index.routes.ts
+++ routes/v1/index.routes.ts
@@ -2,5 +2,6 @@ import { router } from '../router.ts';
 import status from '../../controllers/status.ts';
 
 export const loadV1Routes = () => {
+    router.redirect('/', '/status');
     router.get('/status', status);
 };
