8c237cfc7b1653210f6ac6c83dfe19a30ccaba97
Grama Nicolae <gramanicu@gmail.com>
2022-01-29 18:58:33 +0200

    Added redirect for base url

--- routes/router.ts
+++ routes/router.ts
@@ -2,4 +2,5 @@ import { Router } from 'https://deno.land/x/oak/mod.ts';
 import { apiRouterV1 } from './v1/index.routes.ts';
 
 export const router = new Router()
+    .redirect('/', '/v1')
     .use('/v1', apiRouterV1.routes(), apiRouterV1.allowedMethods());
