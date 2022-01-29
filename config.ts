f344edc0fae2f889b37efd6f38a985ba082a2832
Grama Nicolae <gramanicu@gmail.com>
2022-01-29 12:34:47 +0200

    Deno deploy fix

--- config.ts
+++ config.ts
@@ -1,6 +1,9 @@
 import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
-config({ export: true });
+// Check for DenoDeploy
+if (typeof Deno.readFileSync == 'function') {
+    config({ export: true });
+}
 
 export const APP_HOST = Deno.env.get('APP_HOST') || '127.0.0.1';
 export const APP_PORT = Deno.env.get('APP_PORT') || 4000;
