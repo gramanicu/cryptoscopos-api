07b374609b8249d5b970f16351e81c4664f4268c
Grama Nicolae <gramanicu@gmail.com>
2022-01-29 23:02:21 +0200

    Debug deploy

--- controllers/status.ts
+++ controllers/status.ts
@@ -1,6 +1,10 @@
-import { getPostgresClient, mongoDB, postgresPool } from '../db/db.ts';
+import { getPostgresClient, mongoDB } from '../db/db.ts';
+import { MONGO_VARS, POSTGRES_VARS } from '../config.ts';
 
 const status = ({ response }: { response: any }) => {
+    console.log(MONGO_VARS);
+    console.log(POSTGRES_VARS);
+
     response.body = { msg: 'OK' };
     response.status = 200;
 
