c4564d2e62c26a1d06a8e16601e65d3a2d600a0f
Grama Nicolae <gramanicu@gmail.com>
2022-06-24 14:37:56 +0300

    Small changes

--- src/routes.ts
+++ src/routes.ts
@@ -6,15 +6,16 @@ import AccountsRouter from './routes/accounts.route';
 import PrivateAccountsRouter from './routes/privateAccounts.route';
 import AlertsRouter from './routes/alerts.route';
 import MiscRouter from './routes/misc.route';
+import checkJwt from './middlewares/auth0.middleware';
 
 const router: Router = express.Router();
 
-router.use('/auth', AuthRouter);
-router.use('/private-user', PrivateUserRouter);
+router.use('/auth', checkJwt, AuthRouter);
+router.use('/private-user', checkJwt, PrivateUserRouter);
 router.use('/coins', CoinsRouter);
-router.use('/private-accounts/', PrivateAccountsRouter);
-router.use('/accounts/', AccountsRouter);
-router.use('/alerts', AlertsRouter);
+router.use('/private-accounts/', checkJwt, PrivateAccountsRouter);
+router.use('/accounts/', checkJwt, AccountsRouter);
+router.use('/alerts', checkJwt, AlertsRouter);
 router.use('/', MiscRouter);
 
 export default router;
